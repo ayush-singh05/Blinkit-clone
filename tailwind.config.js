ceebb040e416082b29feb2d980bfcee49487f032
Ayush Singh <ayushsingh2442@gmail.com>
2023-08-31 22:26:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'facebook' : '#1095F4',
+        'instagram':'#FE01D6',
+        'twitter':'#1DA1F3'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
